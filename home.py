b31209bbe0e7ff29f309f4bb9d039b789efad642
sungh7 <sungh7@github.com>
2024-02-07 17:01:09 +0900

    initial commit

---
+++ home.py
@@ -0,0 +1,18 @@
+import streamlit as st 
+
+st.title("title")
+
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("xxx")
+
+tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
+
+with tab_one:
+    st.write("a")
+
+with tab_two:
+    st.write("b")
+
+with tab_three:
+    st.write("c")
